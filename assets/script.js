c5011eee44328b6be923919930b2c04267f8badd
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2023-03-02 13:42:14 +0100

    correct linters

--- assets/script.js
+++ assets/script.js
@@ -19,12 +19,10 @@ function checkClick () {
 }
 
 function getWinner (position) {
-  if (position[0][0] === position[1][0] || position[0][1] === position[1][1] || Math.abs(position[0][0] - position[1][0]) === Math.abs(position[0][1] - position[1][1])) {
+  if (position[0][0] === position[1][0] || position[0][1]  === position[1][1] || Math.abs(position[0][0] - position[1][0]) === Math.abs(position[0][1] - position[1][1])) {
     display.innerHTML= 'A Queen Was attacked'
   } else {
     display.innerHTML = 'A Queen was NOT attacked'
   }
 }
 checkClick()
-// display.innerHTML(checkClick())
-
