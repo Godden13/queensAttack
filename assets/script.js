c27dba11d97342e65ca3970fb1b4d50318c655b2
Godden13 <chrissgodden@gmail.com>
2023-06-21 10:29:33 +0100

    change background button onclick

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
-const btn = document.querySelectorAll('button')
+const btn = document.querySelectorAll('.btn')
 const display = document.querySelector('.display')
+const checkBtn = document.querySelector('.confirm');
 
 const position = []
 
@@ -7,22 +8,25 @@ function checkClick () {
   let count = 0
   btn.forEach((btn, index) => {
     btn.addEventListener('click', () => {
+      btn.style.background = 'brown'
       count += 1
       position.push([Math.floor((index) / 8) + 1, (index % 8)])
       if (count === 2) {
         alert('GAME OVER')
         btn.disabled = true
-        getWinner(position)
       }
+      checkBtn.addEventListener('click', ()=>{
+        getWinner(position)
+      })
     })
   })
 }
 
 function getWinner (position) {
   if (position[0][0] === position[1][0] || position[0][1]  === position[1][1] || Math.abs(position[0][0] - position[1][0]) === Math.abs(position[0][1] - position[1][1])) {
-    display.innerHTML= 'A Queen Was attacked'
+    display.innerHTML= 'A Queen CAN attacked'
   } else {
-    display.innerHTML = 'A Queen was NOT attacked'
+    display.innerHTML = 'A Queen CANNOT attacked'
   }
 }
 checkClick()
